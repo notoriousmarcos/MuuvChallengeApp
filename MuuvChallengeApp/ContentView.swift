5a6ef1a457acaa7fd06850d2428f2f07c1d674d5
Marcos Brito <marcosvcbrito@gmail.com>
2022-07-04 13:10:22 -0300

    Initial Commit

---
+++ MuuvChallengeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MuuvChallengeApp
+//
+//  Created by Marcos Vinicius Brito on 04/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MuuvChallengeApp/MuuvChallengeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuuvChallengeAppApp.swift
+//  MuuvChallengeApp
+//
+//  Created by Marcos Vinicius Brito on 04/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MuuvChallengeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
